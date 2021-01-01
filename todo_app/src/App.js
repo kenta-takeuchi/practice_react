e11a7babca242af77f6a7fed2f85d9ef666aea26
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-01-02 08:03:53 +0900

    todo filter

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
+import TodoFilter from "./components/TodoFilter";
 import useTodo from "./hooks/useTodo";
 
 function App() {
     const {todos, toggleTodo, deleteTodo, addTodo} = useTodo();
+    const [filter, setFilter] = useState('all')
+
+    const handlerFilter = event => {
+        setFilter(event.target.value);
+    }
+
+    const filteredTodos = useMemo(() => {
+        switch (filter) {
+            case 'active':
+                return todos.filter(todo => !todo.completed);
+            case 'completed':
+                return todos.filter(todo => todo.completed);
+            case 'all':
+            default:
+                return todos;
+        }
+    }, [todos, filter])
+
     return (
         <div>
             <h1>Todo List</h1>
+            <TodoFilter selectedFilter={filter} handlerFilter={handlerFilter}/>
             <TodoForm addTodo={addTodo}/>
-            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+            <TodoList todos={filteredTodos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
         </div>
     );
 }
