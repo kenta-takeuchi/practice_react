0707a002c455856a907c96e4b5339636f79ff816
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-01-02 01:26:31 +0900

    for map

--- src/App.js
+++ src/App.js
@@ -10,11 +10,21 @@ const Hello = (props) => {
 
 function App() {
     const isReact = false;
+    const books = [
+        {id: 1, title: 'React'},
+        {id: 2, title: 'Vue'},
+        {id: 3, title: 'Angular'}
+    ]
     return (
         <div className="App">
-            {isReact && <Hello name="React" />}
-            {!isReact && <Hello name="Vue" />}
+            {isReact && <Hello name="React"/>}
+            {!isReact && <Hello name="Vue"/>}
             <p>1 + 2 = {add(1, 2)}</p>
+            <ul>
+                {books.map(book => (
+                    <li key={book.id}>{book.title}</li>
+                ))}
+            </ul>
         </div>
     );
 }
