90bddb7a496644f4630176e55a912abff5c088ea
kenta-takeuchi <49678973+kenta-takeuchi@users.noreply.github.com>
2021-01-02 00:59:45 +0900

    add functino

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+function add(a, b) {
+    return a + b;
+}
+
 function App() {
   return (
     <div className="App">
       Hello World!
+        <p>1 + 2 = {add(1, 2)}</p>
     </div>
   );
 }
